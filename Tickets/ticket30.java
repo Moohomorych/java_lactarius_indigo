4490c7a112074748892378a1df1f47a6984913a6
Moohomorych <134853874+Moohomorych@users.noreply.github.com>
2024-01-19 11:17:24 +0300

    Update ticket30.java

--- Tickets/ticket30.java
+++ Tickets/ticket30.java
@@ -1,3 +1,23 @@
 /*
 Задача на рекурсию: Дано натуральное число N. Выведите слово YES, если число N является точной степенью двойки, или слово NO в противном случае. Операцией возведения в степень пользоваться нельзя!
 */
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        int n = 16;
+        if (isPowerOfTwo(n)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+    private static boolean isPowerOfTwo(int n) {
+        if (n == 1) {
+            return true;
+        } else if (n % 2 == 0 && n > 0) {
+            return isPowerOfTwo(n / 2);
+        } else {
+            return false;
+        }
+    }
+}

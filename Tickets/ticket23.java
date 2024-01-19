08c56f3ac8654548d0f22fd2838cd06c99d14259
Moohomorych <134853874+Moohomorych@users.noreply.github.com>
2024-01-19 11:09:24 +0300

    Update ticket23.java

--- Tickets/ticket23.java
+++ Tickets/ticket23.java
@@ -1,3 +1,48 @@
 /*
 Разработайте класс иерархию классов Геометрическая фигура, Прямоугольник, Круг. Используйте паттерн Фабрика
 */
+
+// Интерфейс для всех геометрических фигур
+interface GeometricShape {
+    void draw();
+}
+// Класс-фабрика для создания геометрических фигур
+class ShapeFactory {
+    public GeometricShape createShape(String type) {
+        if ("Rectangle".equalsIgnoreCase(type)) {
+            return new Rectangle();
+        } else if ("Circle".equalsIgnoreCase(type)) {
+            return new Circle();
+        }
+        // В случае неизвестного типа возвращаем null или выбрасываем исключение
+        return null;
+    }
+}
+// Конкретная реализация прямоугольника
+class Rectangle implements GeometricShape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
+// Конкретная реализация круга
+class Circle implements GeometricShape {
+    @Override
+    public void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+// Пример использования
+public class GeometryApp {
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+        // Создаем прямоугольник с помощью фабрики
+        GeometricShape rectangle = shapeFactory.createShape("Rectangle");
+        // Рисуем прямоугольник
+        rectangle.draw();
+        // Создаем круг с помощью фабрики
+        GeometricShape circle = shapeFactory.createShape("Circle");
+        // Рисуем круг
+        circle.draw();
+    }
+}

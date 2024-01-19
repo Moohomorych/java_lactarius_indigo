41c11c64d646fe4dba184fab5fcc506c436b31e8
Moohomorych <134853874+Moohomorych@users.noreply.github.com>
2024-01-19 11:18:05 +0300

    Update ticket28.java

--- Tickets/ticket28.java
+++ Tickets/ticket28.java
@@ -1,3 +1,21 @@
 /*
 Дано натуральное число N. Вычислите сумму его цифр. При решении этой задачи нельзя использовать строки, списки, массивы (ну и циклы, разумеется).
 */
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        int number = 12345;
+        int sum = calculateSumOfDigits(number);
+        System.out.println("Sum of digits for " + number + " is: " + sum);
+    }
+    // Рекурсивный метод для вычисления суммы цифр числа
+    private static int calculateSumOfDigits(int n) {
+        // Базовый случай: если число состоит из одной цифры, возвращаем это число
+        if (n < 10) {
+            return n;
+        } else {
+            // Рекурсивно вызываем метод для оставшихся цифр числа и складываем с текущей цифрой
+            return n % 10 + calculateSumOfDigits(n / 10);
+        }
+    }
+}
